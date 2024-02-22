12f75b290fa8d09b3eb2e49ca594e672c7edc764
hachemi_benguergoura <h.benguergoura@desolvo.com>
2024-02-22 17:28:15 +0100

    ajout classe disque C

---
+++ Exo_File_System/LocalDisc-C.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_File_System
+{
+    public class LocalDisc_C : ICommonComponent
+    {
+        public string Name { get ; set; }
+
+        public List<ICommonComponent> CommonComponents { get; set; }
+        public int Size { get; private set; }
+
+        public LocalDisc_C(string name)
+        {
+            Name = name;
+            Size = 0;
+        }
+
+        public void AddComponentToDisc(ICommonComponent component)
+        {
+            CommonComponents.Add(component);
+            Size += component.Size;
+        }
+    }
+}
